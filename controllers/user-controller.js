a7856608e3a4091c5021ee18a9d7e6ba941bd250
Travis Geers <armyman1992@hotmail.com>
2022-11-16 14:38:03 -0700

    created index file for models, added reaction schema to thought model, created thought model and user model.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,54 @@
-const mongoose = require('mongoose');
+const { User, Thought } = require('../models');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetwork', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+const userController = {
+    // get all users
+    getUsers(req, res) {
+        User.find({})
+          .select('-__v')
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => res.status(500).json(err));
+    },
 
-module.exports = mongoose.connection;
+    // get single user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+          .populate({
+              path: 'friends',
+              select: '-__v'
+          })
+          .populate({
+              path: 'thoughts',
+              select: '-__v'
+          })
+          .select('-__v')
+          .then(dbUserData => {
+            //   if no user found, send error
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this ID.' }); 
+                return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => res.status(500).json(err));
+    },
+
+    // create a user
+    createUser(req, res) {
+        User.create(req.body)
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => res.status(400).json(err));
+    },
+
+    // update a user
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true})
+          .then(dbUserData => {
+              if(!dbUserData) {
+                  res.status(404).json({ message: 'No user found with this ID.' });
+                  return;
+              }
+              res.json(dbUserData);
+          })
+          .catch(err => res.status(500).json(err));
+    }
+};
