37fcd025bcde88372227172dc9e79efe2128bc23
Travis Geers <armyman1992@hotmail.com>
2022-11-18 23:29:07 -0700

    added user and thought routes, edited thought model and made additions and edits to user and thought controller.

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -50,5 +50,48 @@ const userController = {
               res.json(dbUserData);
           })
           .catch(err => res.status(500).json(err));
+    },
+
+    // delete a user and associated thoughts
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+          .then(dbUserData => {
+              if(!dbUserData) {
+                  res.status(404).json({ message: 'No user found with this ID.' });
+                  return;
+              }
+            //   delete thoughts associated with user
+              return Thought.deleteMany({ _id: { $in: dbUserData.thoughts} });
+          })
+          .then(() => res.json({ message: 'User and associated thoughts deleted.' }))
+          .catch(err => res.status(500).json(err));
+    },
+
+    // add friend to friend list
+    addFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.userId }, { $addToSet: { friends: params.friendId }}, { new: true })
+          .then(dbUserData => {
+              if(!dbUserData) {
+                  res.status(404).json({ message: 'No user found with this ID.' });
+                  return;
+              }
+              res.json(dbUserData);
+          })
+          .catch(err => res.status(500).json(err));
+    },
+
+    // remove friend from friend list
+    removeFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.userId }, { $pull: { friends: params.friendId }}, { new: true })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this ID.'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(500).json(err));
     }
 };
+
+module.exports = userController;
